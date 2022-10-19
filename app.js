cd19cf45a90cf6dbd71c0f4842d371f9bedbe914
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-10-19 15:40:51 -0500

    Changed variable declarations to let instead of const

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 // ${userInputs[x]} .. where X is the index of the user input
 // Increment X by 1 for each new word/phrase
 
-const numberOfQuestions = 15;
+let numberOfQuestions = 15;
 
-const questionCounter = 0
+let questionCounter = 0
 
-const questionArray = [
+let questionArray = [
     "Enter an adjective",
     "Enter a nationality",
     "Enter a person",
@@ -25,21 +25,21 @@ const questionArray = [
     "Enter a number"
 ];
 
-for (let i = questionArray; i >= 0; i--) {
+for (let i = numberOfQuestions; i >= 0; i--) {
     console.log(i);
     questionCounter++;
 };
 
-const userInputs = [];
+let userInputs = [];
 
-const originalStory = `Pizza was invented by a ${userInputs[0]} ${userInputs[1]} chef named ${userInputs[2]}.
+let originalStory = `Pizza was invented by a ${userInputs[0]} ${userInputs[1]} chef named ${userInputs[2]}.
 To make pizza, you need to take a lump of ${userInputs[3]} and make a thin, round ${userInputs[4]} ${userInputs[5]}.
 Then you cover it with ${userInputs[6]} sauce, ${userInputs[7]} cheese, and fresh chopped ${userInputs[8]}. 
 Next you have to bake it in a very hot ${userInputs[9]}. When it is done, cut it into ${userInputs[10]} ${userInputs[11]}.
 Some kids like ${userInputs[12]} pizza the best, but my favorite is the ${userInputs[13]} pizza.
 If I could, I would eat pizza ${userInputs[14]} times a day!`
 
-// console.log(originalStory);
+console.log(originalStory);
 
 
 
