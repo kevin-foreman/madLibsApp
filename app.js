1bd199436a8b10e57a4f8574aedff375757fccfd
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-10-19 15:24:55 -0500

    Cleaned up original story, and added questions array with a for loop to iterate through it

--- app.js
+++ app.js
@@ -3,24 +3,44 @@
 // ${userInputs[x]} .. where X is the index of the user input
 // Increment X by 1 for each new word/phrase
 
-const userInputs = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8", "Question 9", "Question 10", "Question 11", "Question 12"];
+const userInputs = [];
 
-const originalStory = `Pizza was invented by a 'adjective' ${userInputs[0]} 'nationality' ${userInputs[1]} chef named 'person' ${userInputs[2]}.
-To make pizza, you need to take a lump of 'noun' ${userInputs[3]} and make a thin, round 'adjective' ${userInputs[4]} 'noun' ${userInputs[5]}.
-Then you cover it with 'adjective' ${userInputs[6]} sauce, 'adjective' ${userInputs[7]} cheese, and fresh chopped 'plural noun' ${userInputs[8]}. 
-Next you have to bake it in a very hot 'noun' ${userInputs[9]}. When it is done, cut it into 'number' ${userInputs[10]} 'shapes' ${userInputs[11]}.
-Some kids like 'food' ${userInputs[12]} pizza the best, but my favorite is the 'food' ${userInputs[13]} pizza.
-If I could, I would eat pizza 'number' ${userInputs[13]} times a day!`
+const originalStory = `Pizza was invented by a ${userInputs[0]} ${userInputs[1]} chef named ${userInputs[2]}.
+To make pizza, you need to take a lump of ${userInputs[3]} and make a thin, round ${userInputs[4]} ${userInputs[5]}.
+Then you cover it with ${userInputs[6]} sauce, ${userInputs[7]} cheese, and fresh chopped ${userInputs[8]}. 
+Next you have to bake it in a very hot ${userInputs[9]}. When it is done, cut it into ${userInputs[10]} ${userInputs[11]}.
+Some kids like ${userInputs[12]} pizza the best, but my favorite is the ${userInputs[13]} pizza.
+If I could, I would eat pizza ${userInputs[14]} times a day!`
 
-const numberOfQuestions = 12;
+// console.log(originalStory);
+
+const numberOfQuestions = 14;
+
+const questionArray = [
+    "Enter an adjective",
+    "Enter a nationality",
+    "Enter a person",
+    "Enter a noun",
+    "Enter and adjective",
+    "Enter a noun",
+    "Enter an adjective",
+    "Enter an adjective",
+    "Enter a plural noun",
+    "Enter a noun",
+    "Enter a number",
+    "Enter a plural shape",
+    "Enter a food",
+    "Enter a number"
+];
+
+for (let i = questionArray; i >= 0; i--) {
+    console.log(i);
+};
 
 
-console.log(originalStory);
 
 
-
-
-/*
+/* Reference chart for question types
 Question 1: adjective               Question 8: adjective
 Question 2: nationality             Question 9: plural noun
 Question 3: person                  Question 10: noun
