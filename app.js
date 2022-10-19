4f385c9e748361b8a6c75cd7ffe0fd66b2e96454
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-10-19 15:03:06 -0500

    Added entire original story with tag functions and built userInputs array

---
+++ app.js
@@ -0,0 +1,22 @@
+// Start with a variable for the original story with Adjectives, Nouns, and Protagonist name as place-holders for user input
+// Captured userInputs can be used to replace words in the original story with template literals
+// ${userInputs[x]} .. where X is the index of the user input
+// Increment X by 1 for each new word/phrase
+
+const originalStory = `Pizza was invented by a 'adjective' ${userInputs[0]} 'nationality' ${userInputs[1]} chef named 'person' ${userInputs[2]}. 
+To make pizza, you need to take a lump of 'noun' ${userInputs[2]} and make a thin, round 'adjective' ${userInputs[3]} 'noun' ${userInputs[4]}. Then you cover it with 'adjective' ${userInputs[5]} 
+sauce, 'adjective' ${userInputs[6]} cheese, and fresh chopped 'adjective' ${userInputs[7]}. 
+Next you have to bake it in a very hot 'noun' ${userInputs[8]}. When it is done, cut it into 'number' ${userInputs[9]} 'shapes' ${userInputs[10]}.
+Some kids like 'food' ${userInputs[11]} pizza the best, but my favorite is the 'food' ${userInputs[12]} pizza.
+If I could, I would eat pizza 'number' ${userInputs[12]} times a day!`
+
+const userInputs = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8", "Question 9", "Question 10", "Question 11", "Question 12"];
+
+
+
+
+
+
+
+
+// const originalStory = "";
